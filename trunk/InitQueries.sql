b9e34cf42641f7e87002c28d375a1fdd1b6261f8
Unix9N@gmail.com <Unix9N@gmail.com@5b35bf0a-ddaf-ecd5-7847-1f0ae0718e02>
2011-05-31 14:22:57 +0000

    Automated commit 05/31/2011 20:22:55 (matrix property: true)

--- trunk/InitQueries.sql
+++ trunk/InitQueries.sql
@@ -3,3 +3,7 @@ delete from wordlist where id > 0;
 \. H:\Sourcecodes\Web\GREWords\Parser\testquery.sql
 
 drop table wordlist;
+
+insert into wordlist
+	(word, Meaning, Example, wdtype, isprinted)
+	values ('Toll', '(of a bell) Sound with a slow, uniform succession of strokes, as a signal or announcement', 'Tolling like that must be for a governor at least.', 4, 0);
